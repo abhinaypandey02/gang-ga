20ecdb945cdf7dd3ffbbb56733b6c9eccf6d9fa8
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2021-05-16 15:41:55 +0530

    fguy8g

--- src/components/navigationBar/navigation_bar.tsx
+++ src/components/navigationBar/navigation_bar.tsx
@@ -1,19 +1,26 @@
 
-import { Navbar, Nav, Button, Form, FormControl } from "react-bootstrap";
+import React from "react";
+import { Navbar, Nav, Button, Form, FormControl, NavDropdown } from "react-bootstrap";
 
 export default function NavigationBar(){
     return(
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        
-        <Navbar.Brand href="#home" className="mx-2">Navbar</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+        <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="ml-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+ 
+  </Navbar.Collapse>
+</Navbar>
     )
 }
