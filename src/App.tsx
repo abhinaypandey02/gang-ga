e98a45b0c4363054d63374871f9bfca3b2bde5f3
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-05-20 18:47:46 +0530

    ci cd

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,21 @@ import NavigationBar from './components/navigationBar/navigation_bar';
 import AllGymsPage from './pages/allgymsPage/allgyms_page';
 import EachGymPage from './pages/eachgymPage/eachgym_page';
 import LandingPage from './pages/landingPage/landing_page';
-
+import {HashRouter, Switch, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <div className="d-flex flex-column" id='maindiv'>
-      <EachGymPage/>
-      <Footer/>
+        <HashRouter>
+          <Switch>
+            <Route path="/"><LandingPage/></Route>
+            <Route path="/search"><AllGymsPage/></Route>
+            <Route path="/gym"><EachGymPage/></Route>
+          </Switch>
+
+        </HashRouter>
+        <Footer/>
+      
     </div>
     </div>
   );
