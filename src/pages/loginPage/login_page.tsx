931f1692ee90e0e287a036055f5e01824fae7423
Prince Singh <marvelousprince012233@gmail.com>
2022-06-08 16:05:29 +0530

    Navbar login signup design changes

--- src/pages/loginPage/login_page.tsx
+++ src/pages/loginPage/login_page.tsx
@@ -1,53 +1,57 @@
 
+import { LogoDev } from "@mui/icons-material";
 import React from "react";
-import {Button, Form } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router";
 import NavigationBar from "../../components/navigationBar/navigation_bar";
 import { signInWithEmailPassword } from "../../utils/firebase/auth";
 import "./login_page.css";
 
-export default function LoginPage(){
-    const his=useHistory();
-    const {register, handleSubmit}=useForm();
-    function onSubmit(data:any){
-        signInWithEmailPassword(data.email,data.password);
-    }
-    return(
-        <div>
-            <NavigationBar/>
-            <h1 className='display-3 text-light m-3'>Login</h1>
-            <div className="container d-flex flex-column">
-                <div className="col-md-7 mx-auto">
-            <div className="card text-start mx-auto" id='scard'>
-                <div className="row g-0">
-                 
-                  <div className="col">
-                    <div className="card-body">
-                    <Form onSubmit={handleSubmit(onSubmit)}>
-                        <Form.Group controlId="formBasicEmail" className='mb-2'>
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control {...register("email")} type="email" placeholder="Enter email" className='rounded-pill bg-transparent text-light'/>
-                  
-                        </Form.Group>
+export default function LoginPage() {
+  const his = useHistory();
+  const { register, handleSubmit } = useForm();
+  function onSubmit(data: any) {
+    signInWithEmailPassword(data.email, data.password);
+  }
+  return (
+    <div>
+      <NavigationBar />
+      <h2 className='text-light m-3'>Login to VYAYAMSHALA</h2>
+      <div className="container">
+        <div className="col-md-7 mx-auto">
+          <div className="card text-start mx-auto" id='scard'>
+            <div className="row g-0">
 
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control {...register("password")} type="password" placeholder="Password" className='mb-2 rounded-pill bg-transparent text-light'/>
-                        </Form.Group>
-              
-                        <Button type="submit" variant="light" className='rounded-pill my-2'>Login</Button>
-                        </Form>
-                    </div>
-                  </div>
+              <div className="col">
+                <div className="d-flex justify-content-center">
+                  <img src="logo.png" alt="Vyayamshala Logo" />
                 </div>
-             
+                <div className="card-body">
+                  <Form onSubmit={handleSubmit(onSubmit)}>
+                    <Form.Group controlId="formBasicEmail" className='mb-2'>
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control {...register("email")} type="email" placeholder="Enter email" className='rounded-pill bg-transparent text-light' />
+
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control {...register("password")} type="password" placeholder="Password" className='mb-2 rounded-pill bg-transparent text-light' />
+                    </Form.Group>
+
+                    <Button type="submit" variant="dark" className='rounded-pill my-2'>Login</Button>
+                  </Form>
+                </div>
               </div>
-              </div>
-              <h6 className='text-light mx-auto my-2'>Not a Member?<Button onClick={()=>his.push('/signup')} variant="link">Signup</Button>here</h6>
-              
-              </div>
-              
+            </div>
+
+          </div>
         </div>
-    )
+        <h6 className='text-light mx-auto my-2'>Not a Member?<Button onClick={() => his.push('/signup')} variant="link">Signup</Button>here</h6>
+
+      </div>
+
+    </div>
+  )
 }
